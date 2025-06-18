4e29d43464d0202ac2d72596fb09335ef7981ecb
Patrick Davis <207300137+PandasAreSus@users.noreply.github.com>
2025-06-17 20:29:17 -0400

    Basic html layout added

---
+++ script.js
@@ -0,0 +1,12 @@
+// const container = document.querySelector("#container");
+
+// for (let i = 0; i < 4; i++) {
+//     const gridCol = document.createElement("div");
+//     gridCol.classList.add("grid-col-" + i);
+//     for (let j = 0; j < 4; j++) {
+//         const gridRow = document.createElement("button");
+//         gridRow.classList.add("grid-row-" + j);
+//         gridCol.appendChild(gridRow);
+//     }
+//     container.appendChild(gridCol);
+// }
